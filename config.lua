c9f4f53569d6173943be544fefa04a39c79ad419
nosecs <95661721+nosecs@users.noreply.github.com>
2022-01-23 16:30:02 +0100

    Merge With Old System

--- config.lua
+++ config.lua
@@ -1,7 +1,13 @@
 Config = Config or {}
 
 Config.WhitelistedCreators = {
-    "PUTCID",
+    "BPT79951",
+    "LWR55470",
+    "PWD96188",
+    "UNA59325",
+    "WGZ05833",
+    "NVU77833",
+    "AHO58808",
 }
 
 Config.RaceSetupAllowed = true
